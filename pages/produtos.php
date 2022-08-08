6a84b62e568203794930f95fdefe9783e6bdbe78
XavierBruno29 <110863295+XavierBruno29@users.noreply.github.com>
2022-08-08 20:23:23 -0300

    Add files via upload

---
+++ pages/produtos.php
@@ -0,0 +1,83 @@
+<div id="imagemPaginaPrincipal">
+	<h1>CONFIRA NOSSA MARMITAS</h1>
+	<p>AQUI VOCÊ ENCONTRA NOSSO CARDAPIO DE MARMITAS</p>
+	<div class="imgPequena">
+		<div class="imgProduto">
+			<img src="images/spagueti.JPG"><br>		
+		</div>
+		<p class="texto">Spaguetti ao Pesto com Tomates Assados (Vegano)</p>
+		<a href="index.php?pg=5&cod=0"> <p class="comprar"> Ver mais</p></a>
+		
+	</div>
+
+	<div class="imgPequena">
+		<div class="imgProduto">
+			<img src="images/marmita_saudavel.JPG"><br>
+
+		</div>
+		<p class="texto">Marmita Saudável</p>
+		<a href="index.php?pg=5&cod=1">  <p class="comprar"> Ver mais</p></a>
+
+	</div>
+
+	<div class="imgPequena">
+		<div class="imgProduto">
+			<img src="images/marmita_caseira.JPG"><br>
+		</div>
+		<p class="texto">Marmita Caseira</p>
+		<a href="index.php?pg=5&cod=2">  <p class="comprar"> Ver mais</p></a>
+		
+	</div>
+
+	<div class="imgPequena">
+		<div class="imgProduto">
+			<img src="images/spagueti.JPG"><br>
+			
+		</div>
+		<p class="texto">Spaguetti ao Pesto com Tomates Assados (Vegano)</p>
+		<a href="index.php?pg=5&cod=0">  <p class="comprar"> Ver mais</p></a>		
+		
+	</div>
+	
+
+	<div class="imgPequena">
+		<div class="imgProduto">
+			<img src="images/marmita_saudavel.JPG"><br>
+			
+		</div>
+		<p class="texto">Marmita Saudável</p>
+		<a href="index.php?pg=5&cod=1">  <p class="comprar"> Ver mais</p></a>	
+		
+	</div>
+
+	<div class="imgPequena">
+		<div class="imgProduto">
+			<img src="images/marmita_caseira.JPG"><br>
+		</div>
+		<p class="texto">Marmita Caseira</p>
+		<a href="index.php?pg=5&cod=2">  <p class="comprar"> Ver mais</p></a>
+		
+	</div>
+
+	<div class="imgPequena">
+		<div class="imgProduto">
+			<img src="images/marmita_saudavel.JPG"><br>
+			
+		</div>
+		<p class="texto">Marmita Saudável</p>
+		<a href="index.php?pg=5&cod=1">  <p class="comprar"> Ver mais</p></a>	
+		
+	</div>
+
+	<div class="imgPequena">
+		<div class="imgProduto">
+			<img src="images/marmita_caseira.JPG"><br>
+		</div>
+		<p class="texto">Marmita Caseira</p>
+		<a href="index.php?pg=5&cod=2">  <p class="comprar"> Ver mais</p></a>
+		
+	</div>
+
+
+	
+</div>
